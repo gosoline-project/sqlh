5c278bae8bc92e1ad5ed78ad65cbc1268e2d2a77
Sebastian Struß <struss@justtrack.io>
2026-02-10 13:14:06 +0100

    output for query handler now also respects existing transformer function

--- examples/user_crud.go
+++ examples/user_crud.go
@@ -2,39 +2,57 @@ package main
 
 import (
 	"context"
+	"time"
 
 	"github.com/gosoline-project/httpserver"
 	"github.com/gosoline-project/sqlh"
 	"github.com/gosoline-project/sqlr"
 )
 
+type (
+	UserCreateInput struct {
+		Name string `json:"name"`
+	}
+	UserUpdateInput struct {
+		Name string `json:"name"`
+	}
+	User struct {
+		sqlr.Entity[int]
+		Name string
+	}
+	UserOutput struct {
+		Id        int       `json:"id"`
+		Name      string    `json:"name"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+	}
+)
+
 func NewUserCrud() httpserver.RegisterFactoryFunc {
-	return sqlh.WithCrudHandlers[int, User, UserCreateInput, UserUpdateInput](0, "user", sqlh.SimpleTransformer[int, User, UserCreateInput, UserUpdateInput](&UserTransformer{}))
-}
-
-type UserCreateInput struct {
-	Name string `json:"name"`
-}
-
-type UserUpdateInput struct {
-	Name string `json:"name"`
-}
-
-type User struct {
-	sqlr.Entity[int]
-	Name string
+	return sqlh.WithCrudHandlers[int, User, UserCreateInput, UserUpdateInput, UserOutput](0, "user", sqlh.SimpleTransformer[int, User, UserCreateInput, UserUpdateInput, UserOutput](&UserTransformer{}))
 }
 
 type UserTransformer struct{}
 
-func (u UserTransformer) TransformCreate(ctx context.Context, input *UserCreateInput) (*User, error) {
+func (u UserTransformer) TransformCreate(_ context.Context, input *UserCreateInput) (*User, error) {
 	return &User{
 		Name: input.Name,
 	}, nil
 }
 
-func (u UserTransformer) TransformUpdate(ctx context.Context, user *User, input *UserUpdateInput) (*User, error) {
+func (u UserTransformer) TransformUpdate(_ context.Context, user *User, input *UserUpdateInput) (*User, error) {
 	user.Name = input.Name
 
 	return user, nil
+}
+
+func (u UserTransformer) TransformOutput(_ context.Context, user *User) (UserOutput, error) {
+	out := UserOutput{
+		Id:        user.Id,
+		Name:      user.Name,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+	}
+
+	return out, nil
 }
